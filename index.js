d624a8d4dd4843b944e45183691296a93b0c3373
Guillermo de la Iglesia <zguillez@gmail.com>
2015-10-21 19:37:53 +0200

    test

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-var app = require('./lib/sizmek2htmlflash');
+var sizmek2htmlflash, app;
+
+sizmek2htmlflash = require('./lib/sizmek2htmlflash');
+
+app = new sizmek2htmlflash();
+
+module.exports = app;
