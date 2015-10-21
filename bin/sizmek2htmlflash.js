92a45e1e765249b29a8ec38b1e98334142287908
Guillermo de la Iglesia <zguillez@gmail.com>
2015-10-21 20:03:36 +0200

    bin

---
+++ bin/sizmek2htmlflash.js
@@ -0,0 +1,3 @@
+exports.printMsg = function(opts) {
+	console.log("This is a message from the demo package");
+};
--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-var sizmek2htmlflash, app;
-
-sizmek2htmlflash = require('./lib/sizmek2htmlflash');
-
-app = new sizmek2htmlflash();
-
-module.exports = app;
+var app = require('./lib/sizmek2htmlflash');
+app.printMsg();
