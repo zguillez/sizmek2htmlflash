9bb73ba3a8d92f1858cd92355dc8e4ddbe7dca98
Guillermo de la Iglesia <zguillez@gmail.com>
2015-10-21 20:13:01 +0200

    Fix

--- bin/sizmek2htmlflash.js
+++ bin/sizmek2htmlflash.js
@@ -1,3 +1,4 @@
-exports.printMsg = function(opts) {
-	console.log("This is a message from the demo package");
-};
+#!/usr/bin/env node
+
+var app = require('../lib/sizmek2htmlflash');
+app.printMsg();
